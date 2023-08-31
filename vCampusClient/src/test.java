699c17bdbaa5ac68d11854533e38993e469e5c5e
mesakurax <2243155281@qq.com>
2023-09-01 00:36:45 +0800

    更新

--- vCampusClient/src/test.java
+++ vCampusClient/src/test.java
@@ -4,15 +4,20 @@ import entity.*;
 
 public class test {
 
-    public static void main(String[] args)
-    {
-        SocketHelper help=new SocketHelper();
-        help.getConnection(help.ip,help.port);
-        bankSystem bank=new bankSystem(help);
-        rechargeRecord temp=new rechargeRecord(103,"555","1",50,3," ","uæˆ–");
-        if(bank.user_addRecord(temp))
+    public static void main(String[] args) {
+        SocketHelper help = new SocketHelper();
+        help.getConnection(help.ip, help.port);
+        bankSystem bank = new bankSystem(help);
+        rechargeRecord temp = new rechargeRecord(199, "555", "1", 50, 3, " ", "Ê±´úÔÚÕÙ»½");
+ /*       if (bank.user_addRecord(temp))
             System.out.println("uijuhbj");
+    }
+}*/
+        rechargeRecord[] m = bank.admin_querysolved(temp);
+        for (rechargeRecord record : m) {
+            System.out.println(record.getrId() + ", " + record.getuId() + ", " + record.getuName() + ", " + record.getAmount()
+                    + ", " + record.getIsDispose() + ", " + record.getStatus() + ", " + record.getTime());
+        }
 
     }
-
 }
